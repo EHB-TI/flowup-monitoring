3945ff67abc54e218a6b3d04784c457d60c7eb35
= <=>
2021-04-25 14:21:13 +0200

    Exception handler + bug fix

--- script_logging/src/parseXML.py
+++ script_logging/src/parseXML.py
@@ -5,28 +5,18 @@ import xml.etree.ElementTree as ET
 
 
 def parse(data):
-    myroot = ET.fromstring(data)
-    dataArray = []
-    for elements in myroot:
-        for x in elements:
-            dataArray.append(x.text)
-    result = Heartbeat(dataArray[0], dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataArray[5])
-    return format(result)
+    try:
+        myroot = ET.fromstring(data)
+        dataArray = []
+        for elements in myroot:
+            for x in elements:
+                dataArray.append(x.text)
+        result = Heartbeat(dataArray[0], dataArray[1], dataArray[2], dataArray[3], dataArray[4], dataArray[5])
+        return format(result)
+    except Exception:
+        return Heartbeat(str(0),str(0),str(0),str(0),str(0),str(0))
 
 def format(data):
-    new_data = "{} - {} - {}".format(data.nameService,data.RAMload, data.CPUload)
+    new_data = "{} - {} - {}".format(data.origin,data.RAMload, data.CPUload)
     return new_data
 
-data = """
-<heartbeat>
-    <header>
-      <code>2000</code>
-      <origin>FrontEnd</origin>
-      <timestamp>2021-05-25T12:00:00+01:00</timestamp>
-    </header>
-    <body>
-      <nameService>Website</nameService>
-      <CPUload>5.63</CPUload>
-      <RAMload>86.13</RAMload>
-    </body>
-</heartbeat>"""
